053b53a319bbdb6017fe2b8eb7d1c7e48db3b8b3
David <info@david-maus.de>
2018-04-18 15:41:14 +0200

    case

---
+++ config/sfx/sfxAmbientAfrica.hpp
@@ -0,0 +1,129 @@
+
+
+
+	class africancity
+	{
+		name = "africancity";
+		sounds[]={sfxsound};
+		sfxsound[]={"media\sfx\AmbientAfrica\africancity.ogg",15,1,18,1,1,1,0};
+		empty[]= {"",0,0,0,0,0,0,0};
+	};
+
+	class africancity2
+	{
+		name = "africancity2";
+		sounds[]={sfxsound};
+		sfxsound[]={"media\sfx\AmbientAfrica\africancity2.ogg",10,1,18,1,1,1,0};
+		empty[]= {"",0,0,0,0,0,0,0};
+	};
+
+	class africancity3
+	{
+		name = "africancity3";
+		sounds[]={sfxsound};
+		sfxsound[]={"media\sfx\AmbientAfrica\africancity3.ogg",10,1,18,1,1,1,0};
+		empty[]= {"",0,0,0,0,0,0,0};
+	};
+
+	class africancity4
+	{
+		name = "africancity4";
+		sounds[]={sfxsound};
+		sfxsound[]={"media\sfx\AmbientAfrica\africancity4.ogg",10,1,18,1,1,1,0};
+		empty[]= {"",0,0,0,0,0,0,0};
+	};
+
+	class africancity5
+	{
+		name = "africancity5";
+		sounds[]={sfxsound};
+		sfxsound[]={"media\sfx\AmbientAfrica\africancity5.ogg",10,1,18,1,1,1,0};
+		empty[]= {"",0,0,0,0,0,0,0};
+	};
+	class africancity6
+	{
+		name = "africancity6";
+		sounds[]={sfxsound};
+		sfxsound[]={"media\sfx\AmbientAfrica\africancity6.ogg",10,1,18,1,1,1,0};
+		empty[]= {"",0,0,0,0,0,0,0};
+	};
+
+	class africansinging
+	{
+		name = "africansinging";
+		sounds[]={sfxsound};
+		sfxsound[]={"media\sfx\AmbientAfrica\africansinging.ogg",10,1,18,1,1,1,0};
+		empty[]= {"",0,0,0,0,0,0,0};
+	};
+
+	class drums
+	{
+		name = "drums";
+		sounds[]={sfxsound};
+		sfxsound[]={"media\sfx\AmbientAfrica\drums.ogg",10,1,18,1,1,1,0};
+		empty[]= {"",0,0,0,0,0,0,0};
+	};
+
+	class drums2
+	{
+		name = "drums2";
+		sounds[]={sfxsound};
+		sfxsound[]={"media\sfx\AmbientAfrica\drums2.ogg",10,1,18,1,1,1,0};
+		empty[]= {"",0,0,0,0,0,0,0};
+	};
+
+	class bokonews
+	{
+		name = "bokonews";
+		sounds[]={sfxsound};
+		sfxsound[]={"media\sfx\AmbientAfrica\bokonews.ogg",10,1,18,1,1,1,0};
+		empty[]= {"",0,0,0,0,0,0,0};
+	};
+
+	class Britishnews
+	{
+		name = "Britishnews";
+		sounds[]={sfxsound};
+		sfxsound[]={"media\sfx\AmbientAfrica\Britishnews.ogg",10,1,18,1,1,1,0};
+		empty[]= {"",0,0,0,0,0,0,0};
+	};
+
+	class sudannews1
+	{
+		name = "sudannews1";
+		sounds[]={sfxsound};
+		sfxsound[]={"media\sfx\AmbientAfrica\sudannews1.ogg",10,1,18,1,1,1,0};
+		empty[]= {"",0,0,0,0,0,0,0};
+	};
+
+	class sudannews2
+	{
+		name = "sudannews2";
+		sounds[]={sfxsound};
+		sfxsound[]={"media\sfx\AmbientAfrica\sudannews2.ogg",10,1,18,1,1,1,0};
+		empty[]= {"",0,0,0,0,0,0,0};
+	};
+
+	class africansong1
+	{
+		name = "africansong1";
+		sounds[]={sfxsound};
+		sfxsound[]={"media\sfx\AmbientAfrica\africansong1.ogg",10,1,18,1,1,1,0};
+		empty[]= {"",0,0,0,0,0,0,0};
+	};
+
+	class africansong2
+	{
+		name = "africansong2";
+		sounds[]={sfxsound};
+		sfxsound[]={"media\sfx\AmbientAfrica\africansong2.ogg",10,1,18,1,1,1,0};
+		empty[]= {"",0,0,0,0,0,0,0};
+	};
+
+	class africansong3
+	{
+		name = "africansong3";
+		sounds[]={sfxsound};
+		sfxsound[]={"media\sfx\AmbientAfrica\africansong3.ogg",10,1,18,1,1,1,0};
+		empty[]= {"",0,0,0,0,0,0,0};
+	};
